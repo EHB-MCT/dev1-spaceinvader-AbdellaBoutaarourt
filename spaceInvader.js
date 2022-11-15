a524b694cf3b93cbc1c29c140842d4ad8a45efcb
AbdellaBoutaarourt <abdella.boutaarourt@student.ehb.be>
2022-11-15 20:37:49 +0100

    Taak

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,33 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawSpaceinvader();
+
+function drawSpaceinvader() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.rect(75, 300, 50, 50);
+    context.rect(75, 250, 50, 50);
+    context.rect(125, 250, 50, 50);
+    context.rect(75, 150, 50, 50);
+    context.rect(275, 150, 50, 50);
+    context.rect(225, 250, 50, 50);
+    context.rect(275, 250, 50, 50);
+    context.rect(275, 300, 50, 50);
+    context.rect(225, 200, 50, 50);
+    context.rect(125, 200, 50, 50);
+    context.rect(175, 200, 50, 50);
+    context.rect(175, 150, 50, 50);
+    context.fillStyle = "#00ff00";
+    context.fill();
+    
+    
+}
